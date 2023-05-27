70e105464b76b1637b6f9b374edfb00756425efa
DERRICKSONNDAMBUKI <derricksonndambuki@gmail.com>
2023-05-27 10:39:56 +0300

    new tip calculator

--- app/src/main/java/com/example/ncemptycomposeactivitymaterial3/MainActivity.kt
+++ app/src/main/java/com/example/ncemptycomposeactivitymaterial3/MainActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ncemptycomposeactivitymaterial3.ui.theme.NCEmptyComposeActivityMaterial3Theme
 import java.text.NumberFormat
+
+//var amountInput:MutableState<String> = mutableStateOf(0.toString())
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,12 +58,33 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp)
                 .align(alignment = Alignment.Start)
         )
+        EditNumberField(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
         Text(
-            text = stringResource(id = R.string.tip_amount, "$0.00"),
+            text = stringResource(id = R.string.tip_amount, "Ksh. 0.00"),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
     }
+}
+
+@Composable
+fun EditNumberField(modifier: Modifier = Modifier) {
+    var amountInput by remember {
+        mutableStateOf("")
+    }
+    BasicTextField(
+        value = amountInput,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        onValueChange = { amountInput = it },
+//        label ={ Text(text = stringResource(id = R.string.bill_amount))},
+        cursorBrush = Brush.sweepGradient(),
+        modifier = modifier
+    )
 }
 
 private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
